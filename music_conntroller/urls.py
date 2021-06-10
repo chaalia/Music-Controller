a323d83e2a366c27a14efcbb34b37ace08cf603a
RjabChaalia <rejeb.chaalia@gmail.com>
2021-06-10 14:25:08 +0100

    frontend1

--- music_conntroller/urls.py
+++ music_conntroller/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('api.urls'))
+    url('api', include('api.urls')),
+    url('', include('frontend.urls'))
 ]
