4db6e4bb41295898c319406c58f3b846f85cfc5c
RjabChaalia <rejeb.chaalia@gmail.com>
2021-06-10 17:06:28 +0100

    add home page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage"
 
 export default class App extends Component {
   constructor(props) {
@@ -7,9 +8,9 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1> hello </h1>;
+    return <HomePage />;
   }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Rjab"/>, appDiv);
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+
+export default class HomePage extends Component{
+constructor(props){
+    super(props);
+    }
+    render(){
+    return <p>this is the home page</p>;
+    }
+}
