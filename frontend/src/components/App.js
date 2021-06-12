aaae326064d6499fb68b326332f23fca537aa29b
RjabChaalia <rejeb.chaalia@gmail.com>
2021-06-12 21:14:07 +0100

    fix project

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage"
-
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <HomePage />;
+    return (
+      <div>
+    <HomePage />;
+    <RoomJoinPage />;
+    <CreateRoomPage />
+    </div>)
   }
 }
 
-const appDiv = document.getElementById("app");
+const appDiv = document.getElementById("root");
 render(<App name="Rjab"/>, appDiv);
---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+
+export default class RoomJoinPage extends Component{
+constructor(props){
+    super(props);
+    }
+    render(){
+    return <p>it is the room join page</p>;
+    }
+}
