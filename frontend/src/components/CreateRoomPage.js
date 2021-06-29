3fe7ee7b4ce5d3fb09556f106f402fbd95287730
RjabChaalia <rejeb.chaalia@gmail.com>
2021-06-29 17:36:26 +0100

    add to create room component

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -9,14 +9,30 @@ import {Link} from "react-router-dom";
 import Radio from "@material-ui/core/Radio  ";
 import RadioGroup from "@material-ui/core/RadioGroup  ";
 import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormLabelControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 export default class CreateRoomPage extends Component {
     defaultVotes = 2 ;
     constructor(props) {
         super(props);
+        this.state = {
+            GuestCanPause =true,
+            VotesToSkip = this.defaultVotes === 'true' ? true : false,
+        };
       }
+
+    handleVotesChanged(e) {
+        this.setState({
+            VotesToSkip: e.target.value
+        })
+    };
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            GuestCanPause: e.target.value
+        })
+    }
 
   render() {
     return (
@@ -33,24 +49,54 @@ export default class CreateRoomPage extends Component {
                     Guest Control of playback state
                 </div>
              </FormHelperText>
-             <RadioGroup row defaultValue="true"
-                <FormControlLabel value="true"
-                control={Radio color="primary"}
-                label="Play / Pause"
+             <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+             <FormControlLabel
+                value="true"
+                control={<Radio color="primary" />}
+                label="Play/Pause"
                 labelPlacement="bottom"
-                />
-
-             <RadioGroup row defaultValue="false"
-                <FormControlLabel value="secondary"
-                control={Radio color="primary"}
-                label="No control"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="No Control"
                 labelPlacement="bottom"
-                />
-
-             </RadioGroup>
-         </FormControl>
-    </Grid>
-    </Grid>
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type="number"
+              onChange={this.handleVotesChange}
+              defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              <div align="center">Votes Required To Skip Song</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleRoomButtonPressed}
+          >
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -6,6 +6,6 @@ export default class RoomJoinPage extends Component {
   }
 
   render() {
-    return <p>This is the room join page</p>;
+    return <p>This is the room joins page</p>;
   }
 }
